ebd2dd2f287aa32a9ea72b19b20d98f9a893cbbe
thelamer <ryankuba@gmail.com>
2019-11-27 19:48:48 -0800

    adding gnome assets

--- settings.sh
+++ settings.sh
@@ -1,5 +1,7 @@
-URL="https://osdn.net/projects/manjaro/storage/architect/18.1.0/manjaro-architect-18.1.0-stable-x86_64.iso"
+URL="https://osdn.net/projects/manjaro/storage/gnome/18.1.0/manjaro-gnome-18.1.0-stable-x86_64.iso"
 TYPE=file
 CONTENTS="\
 manjaro/x86_64/livefs.sfs|livefs.sfs
-manjaro/x86_64/rootfs.sfs|rootfs.sfs"
+manjaro/x86_64/rootfs.sfs|rootfs.sfs
+manjaro/x86_64/mhwdfs.sfs|mhwdfs.sfs
+manjaro/x86_64/desktopfs.sfs|desktopfs.sfs"
