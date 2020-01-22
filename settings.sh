33a7845d59fe8e50ca43de711c23c17425c21089
thelamer <ryankuba@gmail.com>
2020-01-22 12:29:16 -0800

    need to pull out all files from desktop based distros

--- settings.sh
+++ settings.sh
@@ -3,6 +3,8 @@ TYPE=file
 CONTENTS="\
 manjaro/x86_64/livefs.sfs|livefs.sfs
 manjaro/x86_64/rootfs.sfs|rootfs.sfs
+manjaro/x86_64/mhwdfs.sfs|mhwdfs.sfs
+manjaro/x86_64/desktopfs.sfs|desktopfs.sfs
 boot/initramfs-x86_64.img|initrd
 boot/vmlinuz-x86_64|vmlinuz"
 EXTRACT_INITRD="true"
