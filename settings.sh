7adc5349c4bfd4db016388505a5e12a75a2f802c
netboot-ci <netboot-ci@netboot.xyz>
2020-12-15 04:35:04 +0000

    Updating workflow automation

--- settings.sh
+++
@@ -1,5 +0,0 @@
-URL="https://osdn.net/projects/manjaro/storage/architect/18.1.0/manjaro-architect-18.1.0-stable-x86_64.iso"
-TYPE=file
-CONTENTS="\
-manjaro/x86_64/livefs.sfs|livefs.sfs
-manjaro/x86_64/rootfs.sfs|rootfs.sfs"
