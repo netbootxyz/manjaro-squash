766846109036b04d31962b0eb2a238ceb11140fa
Antony Messerli <antony@mes.ser.li>
2021-04-17 09:56:33 -0500

    Bump version

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://sourceforge.net/projects/manjarolinux/files/xfce/21.0/manjaro-xfce-REPLACE_VERSION.iso"
+URL="https://sourceforge.net/projects/manjarolinux/files/xfce/21.0.1/manjaro-xfce-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 manjaro/x86_64/livefs.sfs|livefs.sfs
