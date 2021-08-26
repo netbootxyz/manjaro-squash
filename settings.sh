bd572e1d08448a43a0cfd574e409b5125d6780c5
Antony Messerli <antony@mes.ser.li>
2021-08-26 00:22:18 -0500

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://sourceforge.net/projects/manjarolinux/files/kde/21.0.7/manjaro-kde-REPLACE_VERSION.iso"
+URL="https://sourceforge.net/projects/manjarolinux/files/kde/21.1.0/manjaro-kde-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 manjaro/x86_64/livefs.sfs|livefs.sfs
