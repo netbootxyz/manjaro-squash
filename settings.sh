c111ae1030abb27bdb3ee19b52f998c73330021d
Antony Messerli <antony@mes.ser.li>
2021-10-07 17:07:45 -0500

    Bump manjaro

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://sourceforge.net/projects/manjarolinux/files/xfce/21.1.0/manjaro-xfce-REPLACE_VERSION.iso"
+URL="https://sourceforge.net/projects/manjarolinux/files/xfce/21.1.4/manjaro-xfce-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 manjaro/x86_64/livefs.sfs|livefs.sfs
