5eba52dba806dcb2c01314ee785f9f12fa15f34f
Antony Messerli <antony@mes.ser.li>
2022-06-20 17:59:58 -0500

    Update version checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://manjaro.org/downloads/official/kde/ |awk -F '(manjaro-kde-|.iso)' '/manjaro-kde-/ {print $2;exit}')
+VERSION=$(curl -sL https://manjaro.org/downloads | awk -F '(manjaro-kde-|.iso)' '/manjaro-kde-/ {print $2;exit}')
 echo "${VERSION}"
