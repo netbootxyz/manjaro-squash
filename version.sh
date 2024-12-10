f4e8023ef6b9ef34e13043cc7fd73ac0583276d7
Antony Messerli <antony@mes.ser.li>
2024-12-09 23:47:55 -0600

    Bump version checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://manjaro.org/products/download/x86 |awk -F '(manjaro-gnome-|.iso)' '/manjaro-gnome-/ {print $2;exit}')
+VERSION=$(curl -sL https://manjaro.org/products/download/x86 |awk -F '(manjaro-gnome-|.iso)' '/manjaro-gnome-/ {print $4;exit}')
 echo "${VERSION}"
