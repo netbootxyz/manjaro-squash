956465b5cf27737727166d5ee505835be96e4869
Antony Messerli <antony@mes.ser.li>
2024-12-09 23:47:10 -0600

    Bump checker

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://manjaro.org/products/download/x86 |awk -F '(manjaro-xfce-|.iso)' '/manjaro-xfce-/ {print $2;exit}')
+VERSION=$(curl -sL https://manjaro.org/products/download/x86 |awk -F '(manjaro-xfce-|.iso)' '/manjaro-xfce-/ {print $3;exit}')
 echo "${VERSION}"
