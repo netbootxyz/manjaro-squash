d43c7f8c94168fc46d389903b0787c4796a5e725
Antony Messerli <antony@mes.ser.li>
2024-12-09 23:43:17 -0600

    Update download url

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://manjaro.org/downloads |awk -F '(manjaro-xfce-|.iso)' '/manjaro-xfce-/ {print $2;exit}')
+VERSION=$(curl -sL https://manjaro.org/products/download/x86 |awk -F '(manjaro-xfce-|.iso)' '/manjaro-xfce-/ {print $2;exit}')
 echo "${VERSION}"
